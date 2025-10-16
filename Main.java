47167c2303ca17dd1ae1ad73018e3b0315256fcc
naresh-876 <nareshblueboy12@gmail.com>
2025-10-17 04:10:39 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.rp.todo;
+
+import com.rp.todo.ui.ConsoleApp;
+
+public class Main {
+    // Entry point
+    public static void main(String[] args) {
+        new ConsoleApp().run();
+    }
+}
